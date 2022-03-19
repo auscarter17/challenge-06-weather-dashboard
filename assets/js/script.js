e7a19cc72e0e8bbdafa9434a8ceb3b1a4480d777
Austin Carter <98193003+auscarter17@users.noreply.github.com>
2022-03-18 23:35:27 -0400

    added uv index display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 const apiKey = "744a9764e2e344e5caf523b79e9bef07"
-var city = ""
+var city = "";
+var cityLat = "";
+var cityLon = "";
 var cityFormEl = document.querySelector("#city-form");
 var cityInputEl = document.querySelector("#cityname");
 var weatherContainerEl = document.querySelector("#weather-container");
@@ -16,13 +18,13 @@ var getWeather = function(city) {
         response.json().then(function(data) {
             // pass response data to dom function
             console.log(data);
-            displayWeather(data);
+            displayWeather(data, city);
         });
     }
     else {
         alert("There was a problem with your request!");
     }
-});
+  });
 }
     
 var displayWeather = function (weather, searchCity) {
@@ -38,17 +40,69 @@ var displayWeather = function (weather, searchCity) {
   weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
   citySearchTerm.appendChild(weatherIcon);
 
-  
+  var temperatureEl = document.createElement("span");
+  temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F";
+  temperatureEl.classList = "list-group-item"
+ 
+  var humidityEl = document.createElement("span");
+  humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
+  humidityEl.classList = "list-group-item"
 
-  
-  //citySearchTerm.textContent = city;
-  //var tempCurrent = (data.main.temp);
+  var windSpeedEl = document.createElement("span");
+  windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+  windSpeedEl.classList = "list-group-item"
 
+  weatherContainerEl.appendChild(temperatureEl);
 
+  weatherContainerEl.appendChild(humidityEl);
 
+  weatherContainerEl.appendChild(windSpeedEl);
 
+  var cityLat = weather.coord.lat;
+  var cityLon = weather.coord.lon;
+  getUvData(cityLat,cityLon)
 }
 
+
+
+var getUvData = function(cityLat,cityLon) {
+  // use API to find latitude/longitude of searched city
+    
+    var apiUvUrl = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + cityLat + "&lon=" + cityLon + "&appid=" + apiKey + "&cnt=1";
+    
+    fetch(apiUvUrl)
+    .then(function(response) {
+      response.json().then(function(data){
+        displayUvIndex(data)
+      });
+    });
+} 
+    
+var displayUvIndex = function(data) {
+  var uvIndexEl = document.createElement("div");
+    uvIndexEl.textContent = "UV Index: "
+    uvIndexEl.classList = "list-group-item"
+        
+    uvIndexSig = document.createElement("span")
+    uvIndexSig.textContent = data[0].value;
+        
+    console.log(data[0].value);
+
+    if(data[0].value <=2){
+    uvIndexSig.classList = "favorable"
+        
+    }else if(data[0].value >2 && data[0].value<=8){
+    uvIndexSig.classList = "moderate "
+    
+    }else if(data[0].value >8){
+    uvIndexSig.classList = "severe"
+    };
+  
+
+  uvIndexEl.appendChild(uvIndexSig);
+  //append index to current weather
+  weatherContainerEl.appendChild(uvIndexEl);
+}
 
 var formSubmitHandler = function(event) {
   event.preventDefault();
